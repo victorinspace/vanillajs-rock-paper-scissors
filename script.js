043db71342f20bf97f1ad78434bdc5cb4e80d36c
Victor <victorv24@gmail.com>
2021-03-24 18:28:11 -0700

    Player gets visual feedback when selecting a weapon and can trigger a page refresh. TODO: refresh doesn't work.

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const eventListener = function (e) {
-  console.log(e);
+const activateGame = function (e) {
+  e.preventDefault;
+  let weaponSelected = e.target.id;
+  console.log(` Weapon Selected: ${weaponSelected}`);
+
+  let showWeapons = (document.querySelector(".game-table").innerHTML = `
+    <div>
+      <h1 class="show-weapon-selected">${weaponSelected}</h1>
+    </div>
+    <div>
+      <h1 class="AI-selected">Your enemy has selected: WEAPON_ITEM</h1>
+    </div>
+  `);
 };
-document.getElementById("rock").addEventListener("click", eventListener);
-document.getElementById("paper").addEventListener("click", eventListener);
-document.getElementById("scissors").addEventListener("click", eventListener);
+
+const resetGame = function (e) {
+  e.preventDefault;
+  console.log("game reset");
+  document.querySelector(".game-table").innerHTML = `
+    <div id="weapon-locker" class="weapon-locker">
+      <span id="rock" class="weapon">Rock</span>
+      <span id="paper" class="weapon">Paper</span>
+      <span id="scissors" class="weapon">Scissors</span>
+    </div>
+  `;
+};
+
+let rock = document.getElementById("rock");
+rock.addEventListener("click", activateGame);
+
+let paper = document.getElementById("paper");
+paper.addEventListener("click", activateGame);
+
+let scissors = document.getElementById("scissors");
+scissors.addEventListener("click", activateGame);
+
+let resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
