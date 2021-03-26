b80b1fd65e1761f5a87527b023e43ca6be243260
Victor <victorv24@gmail.com>
2021-03-25 20:24:23 -0700

    Player can completely restart the game.

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
+// TODO:
+// - Generate AI response / weapon select
+// - Provide player feedback:
+//     - What did they pick?
+//     - What did the computer pick?
+//     - What was the outcome?
+
+const generateAISelection = function () {
+  return Math.floor(Math.random() * Math.floor(3));
+};
+
 const activateGame = function (e) {
   e.preventDefault;
-  let weaponSelected = e.target.id;
-  console.log(` Weapon Selected: ${weaponSelected}`);
 
-  let showWeapons = (document.querySelector(".game-table").innerHTML = `
-    <div>
-      <h1 class="show-weapon-selected">${weaponSelected}</h1>
-    </div>
-    <div>
-      <h1 class="AI-selected">Your enemy has selected: WEAPON_ITEM</h1>
-    </div>
-  `);
+  let playerSelectedWeapon = e.target.id;
+
+  console.log(`Weapon Selected: ${playerSelectedWeapon}`);
+
+  let gameTable = document.getElementById("weapon-locker");
+  gameTable.style.display = "none";
 };
 
 const resetGame = function (e) {
   e.preventDefault;
-  console.log("game reset");
-  document.querySelector(".game-table").innerHTML = `
-    <div id="weapon-locker" class="weapon-locker">
-      <span id="rock" class="weapon">Rock</span>
-      <span id="paper" class="weapon">Paper</span>
-      <span id="scissors" class="weapon">Scissors</span>
-    </div>
-  `;
+
+  console.log("Game Reset");
+
+  let gameTable = document.getElementById("weapon-locker");
+
+  if (gameTable.style.display === "none") {
+    gameTable.style.display = "flex";
+  }
 };
 
 let rock = document.getElementById("rock");
